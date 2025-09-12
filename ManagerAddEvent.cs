49845d287c7c01399a4c055cf4c5d0d02c2b66e1
Kameron Wheeler <kameronwheeler@icloud.com>
2025-09-12 14:09:59 -0400

    ManagerAddEvent

---
+++ ManagerAddEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace groupProject
+{
+    public partial class ManagerAddEvent : Form
+    {
+        public ManagerAddEvent()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
