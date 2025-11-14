f828fc5c15d9fc4c2a03682142af67979df2fca1
Kameron Wheeler <kameronwheeler@icloud.com>
2025-11-14 11:30:26 -0500

    CodeFile Change

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -16,7 +16,7 @@ namespace groupProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new UserMenu());
+            Application.Run(new ManagerAddEvent());
 
         }
     }
