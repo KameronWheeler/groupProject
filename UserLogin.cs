31cc84bd328b83bf6df353a5104855b03f7e8285
Jonathan Bailey <jonathan_bailey115@mymail.eku.edu>
2025-11-14 12:20:19 -0500

    Login page works

--- UserLogin.cs
+++ UserLogin.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace groupProject
 {
@@ -38,25 +41,42 @@ namespace groupProject
         {
             string username = usernameTextBox.Text;
             string password = passwordTextBox.Text;
-            // Simple hardcoded authentication for demonstration purposes
-            if (username == "admin" && password == "password")
-            {
-                // Successful login
-                UserMenu userMenu = new UserMenu();
-                userMenu.Show();
-                this.Hide();
-            }
-            else
-            {
-                // Failed login
-                label2.Text = "Username/Password incorrect.";
 
-                UserMenu userMenu = new UserMenu();
-                userMenu.Show();
-                this.Hide();
+            string connStr =
+            "server=csitmariadb.eku.edu;user=student;database=csc340_db;port=3306;password=Maroon@21?;";
+            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                string sql = "SELECT * FROM groupjnk_user WHERE Name=@name";
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(sql,
+                conn);
+                cmd.Parameters.AddWithValue("@name", username);
+                MySqlDataReader myReader = cmd.ExecuteReader();
+                if (myReader.Read())
+                {
+                    if (password == myReader["Password"].ToString())
+                    {
+                        // Successful login
+                        UserMenu userMenu = new UserMenu();
+                        userMenu.Show();
+                        this.Hide();
+                    } else
+                    {
+                        // Failed login
+                        label2.Text = "Username/Password incorrect.";
+                    }
+                }
+                myReader.Close();
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            conn.Close();
+            Console.WriteLine("Done.");
 
-            //something new
         }
     }
 }
